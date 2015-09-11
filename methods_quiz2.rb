6068757e59c2a9c93bf339e42ba83eeddb261f59
Naoki Yahashi <yahashinj@s.dcsdk12.org>
2015-09-11 09:45:49 -0600

    fixed the tests

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,17 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
+	def without_doubles(roll_one, roll_two)
+		if doubles?(roll_one, roll_two) 
+			if roll_two == 6
+				roll_two = 1
+			else
+				roll_two = roll_two + 1
+			end
+		end
+	end
 	
+	def doubles?(n,m)
+		n - m == 0
+	end
 
 end

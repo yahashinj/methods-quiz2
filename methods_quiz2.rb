877f56ac16c411b3312ae7e500c782df59d95938
Naoki Yahashi <yahashinj@s.dcsdk12.org>
2015-09-11 10:07:45 -0600

    finished max_maybe

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,7 +1,7 @@
 module MethodsQuiz2
 	
-	def without_doubles(roll_one, roll_two)
-		if doubles?(roll_one, roll_two) 
+	def without_doubles(roll_one, roll_two, no_doubles)
+		if doubles?(roll_one, roll_two) && no_doubles
 			if roll_two == 6
 				roll_two = 1
 			else
@@ -13,6 +13,22 @@ module MethodsQuiz2
 	
 	def doubles?(n,m)
 		n - m == 0
+	end
+
+	def max_maybe(x, y)
+		if x > y
+			if x % 5 == y % 5
+				return y
+			end
+			return x
+		elsif y > x
+			if x % 5 == y % 5
+				return x
+			end
+			return y
+		elsif x == y
+			return 0
+		end
 	end
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -13,9 +13,21 @@ class MethodsQuizTest < MiniTest::Test
 
 	# TODO - write tests here
 	def test_without_doubles
-		assert_equal 3, @m.without_doubles(1,1)
-		assert_equal 4, @m.without_doubles(1,3)
-		assert_equal 7, @m.without_doubles(6,6)
-		assert_equal 11, @m.without_doubles(5,6)
+		assert_equal 3, @m.without_doubles(1, 1, true)
+		assert_equal 4, @m.without_doubles(1, 3, true)
+		assert_equal 7, @m.without_doubles(6,6, true)
+		assert_equal 11, @m.without_doubles(5,6, false)
+		assert_equal 12, @m.without_doubles(6,6, false)
+	end
+
+	def test_max_maybe
+		assert_equal 10, @m.max_maybe(10, 7)
+		assert_equal 5, @m.max_maybe(10, 5)
+		assert_equal 0, @m.max_maybe(13, 13)
+		assert_equal 20, @m.max_maybe(17, 20)
+	end
+
+	def squirrels_playing?
+		
 	end
 end

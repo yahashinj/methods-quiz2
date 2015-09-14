2e31fbba0d17d30821ebba69a436ccdef780829f
Naoki Yahashi <yahashinj@s.dcsdk12.org>
2015-09-14 10:21:05 -0600

    Finished red ticket

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -31,7 +31,23 @@ module MethodsQuiz2
 		end
 	end
 
-	def squirrels_playing?(temp, summer)
+	def squirrels_play?(temp, summer)
 		( temp >= 60 && temp <= 90 ) || ( summer && temp <= 100 && temp >= 60)
+	end
+
+	def red_ticket(j, k, l)
+		if equals_to(j, k, l, 2)
+			return 10
+		elsif equals_to(j, k, l, 1) || equals_to(j, k, l, 0)
+			return 5
+		elsif b != a && c!= a
+			return 1
+		else
+			return 0
+		end
+	end
+
+	def equals_to(val1, val2, val3, y)
+		val1 == y && val2 && y && val3 == y
 	end
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -4,43 +4,82 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative 'methods_quiz2'
 
-class MethodsQuizTest < MiniTest::Test
+class QuizTest < MiniTest::Test
+	i_suck_and_my_tests_are_order_dependent!
+
 	def setup
 		@m = Class.new do
      include MethodsQuiz2
    	end.new
 	end
+end
 
-	# TODO - write tests here
-	def test_without_doubles
-		assert_equal 3, @m.without_doubles(1, 1, true)
-		assert_equal 4, @m.without_doubles(1, 3, true)
-		assert_equal 7, @m.without_doubles(6,6, true)
-		assert_equal 11, @m.without_doubles(5,6, false)
-		assert_equal 12, @m.without_doubles(6,6, false)
-	end
-
-	def test_max_maybe
-		assert_equal 10, @m.max_maybe(10, 7)
-		assert_equal 5, @m.max_maybe(10, 5)
-		assert_equal 0, @m.max_maybe(13, 13)
-		assert_equal 20, @m.max_maybe(17, 20)
-	end
-
-		def test_squirrels_playing?
-		assert_equal true, @m.squirrels_playing?(60, false)
-		assert_equal true, @m.squirrels_playing?(90, false)
-		assert_equal false, @m.squirrels_playing?(95, false)
-		assert_equal true, @m.squirrels_playing?(95, true)
-	end
-
-		def test_red_ticket
-			assert_equal 10, @m.red_ticket(2, 2, 2)
-			assert_equal 1, @m.red_ticket(1, 2, 2)
-			assert_equal 5, @m.red_ticket(1, 1, 1)
-			assert_equal 5, @m.red_ticket(0, 0, 0)
-			assert_equal 0, @m.red_ticket(0, 0, 1)
-			assert_equal 1, @m.red_ticket(2, 0, 1)
-			assert_equal 0, @m.red_ticket(2, 0, 2)
+module MethodsQuiz2Tests
+	
+	class WithoutDoubles < QuizTest
+		def test_not_a_double
+			assert_equal 4, @m.without_doubles(1,3, false)
+			assert_equal 5, @m.without_doubles(3,2, true)
 		end
+		def test_double_without_no_doubles
+			assert_equal 8, @m.without_doubles(4, 4, false)
+			assert_equal 12, @m.without_doubles(6, 6, false)
+		end
+		def test_double_with_no_doubles
+			assert_equal 9, @m.without_doubles(4, 4, true)
+			assert_equal 3, @m.without_doubles(1, 1, true)
+		end
+		def test_wrap_around
+			assert_equal 7, @m.without_doubles(6, 6, true)
+		end
+	end
+
+	class MaxMaybe < QuizTest
+		def test_same_values
+			assert_equal 0, @m.max_maybe(5, 5)
+			assert_equal 0, @m.max_maybe(-6, -6)
+		end
+		def test_remainder_mod_5
+			assert_equal 5, @m.max_maybe(10, 5)
+			assert_equal 4, @m.max_maybe(4, 29)
+			assert_equal -10, @m.max_maybe(-5, -10)
+		end
+		def test_a_is_larger
+			assert_equal 10, @m.max_maybe(10, 4)
+		end
+		def test_b_is_larger
+			assert_equal 6, @m.max_maybe(2, 6)
+		end
+		def test_with_negatives
+			assert_equal -1, @m.max_maybe(-1, -4)
+			assert_equal -1, @m.max_maybe(-4, -1)
+		end
+	end
+
+	class SquirrelsPlay < QuizTest
+		def test_just_right_temp
+			assert_equal true, @m.squirrels_play?(65, false)
+			assert_equal true, @m.squirrels_play?(65, true)
+		end
+		def test_low_temp
+			assert_equal false, @m.squirrels_play?(55, true)
+			assert_equal false, @m.squirrels_play?(55, false)
+		end
+		def test_limit
+			assert_equal true, @m.squirrels_play?(60, true)
+			assert_equal true, @m.squirrels_play?(60, false)
+			assert_equal false, @m.squirrels_play?(59, true)
+			assert_equal false, @m.squirrels_play?(59, false)
+			assert_equal false, @m.squirrels_play?(91, false)
+		end
+		def test_summer
+			assert_equal true, @m.squirrels_play?(91, true)
+			assert_equal false, @m.squirrels_play?(91, false)
+		end
+		def test_summer_limit
+			assert_equal true, @m.squirrels_play?(100, true)
+			assert_equal false, @m.squirrels_play?(101, true)
+		end
+	end
+
 end
